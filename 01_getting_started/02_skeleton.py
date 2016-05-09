ffa3081f4a74a694e0642718e265614afe3cbd3f
pythonstory <pincoin.info@gmail.com>
2016-05-10 03:16:59 +0900

    01-02 파이썬 소스 기본 뼈대

---
+++ 01_getting_started/02_skeleton.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+표준 라이브러리 import
+서드 파티 라이브러리 impport
+개발자 사용자 정의 모듈 import
+상대경로 import는 명시적으로 import
+"""
+import sys
+
+# 클래스 및 함수의 정의
+
+if __name__ == '__main__':
+    print('Hello Python!')
--- 01_getting_started/README.md
+++ 01_getting_started/README.md
@@ -1,4 +1,8 @@
 # 파이썬 시작하기
 
 1. 파이썬 버전 확인
- [01_version.py](01_version.py)
+
+    [01_version.py](01_version.py)
+1. 파이썬 소스 기본 뼈대
+
+    [02_skeleton.py](02_skeleton.py)
