78c1560afd9bf1680ca4d3ade6c198c2d7201b90
pythonstory <pincoin.info@gmail.com>
2016-05-10 03:52:38 +0900

    01-02 파이썬 소스 기본 뼈대 구조화

--- 01_getting_started/02_skeleton.py
+++ 01_getting_started/02_skeleton.py
@@ -6,9 +6,26 @@
 개발자 사용자 정의 모듈 import
 상대경로 import는 명시적으로 import
 """
-import sys
+import sys, os, traceback
+
 
 # 클래스 및 함수의 정의
+def main():
+    global options, args
+
+    # 메인 작업 처리
+    print('Hello Python!')
+
 
 if __name__ == '__main__':
-    print('Hello Python!')
+    try:
+        main()
+        sys.exit(0)
+    except KeyboardInterrupt as e:
+        raise e
+    except SystemExit as e:
+        raise e
+    except Exception as e:
+        print(str(e))
+        traceback.print_exc()
+        os._exit(1)
